3046ee8b20299026b1c29fd3a1ce000b7ef541c5
oskar <ooskar@gmx.de>
2023-01-05 18:45:44 +0100

    stash

---
+++ Lib/GameEntities/Boat.cs
@@ -0,0 +1,37 @@
+﻿namespace Lib.GameEntities
+{
+    public class Boat
+    {
+        public Location Start { get; set; }
+        public Location End { get; set; }
+    
+        public int Length 
+        { 
+            get 
+            {
+                return Direction switch
+                {
+                    Direction.HORIZONTAL => Math.Abs(Start.XAsInt - End.XAsInt),
+                    Direction.VERTICAL => Math.Abs(Start.Y - End.Y),
+                    _ => throw new ArgumentException("A boat can only have a length, if it is placed horizontal or vertical!"),
+                };
+            } 
+        }
+
+        public Direction Direction
+        {
+            get
+            {
+                if(Start.X == End.X) return Direction.HORIZONTAL;
+                if(Start.Y == End.Y) return Direction.VERTICAL;
+                return Direction.DIAGONAL;
+            }
+        }
+
+        public Boat(Location start, Location end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+}
